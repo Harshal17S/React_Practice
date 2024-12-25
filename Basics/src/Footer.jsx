26c8460e1d91ff076120c24a5db66515a9cdcb6b
Harshal17S <harshal.ingle17@gmail.com>
2024-12-25 20:19:54 +0530

    first

---
+++ Basics/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+ return(
+    <footer>
+    <p>&copy;{new Date().getFullYear()} Website</p>
+         <h1>Hello</h1>
+    </footer>
+   
+ );
+}
+
+export default Footer;
---
+++ Basics/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+  return(
+    <header>
+        <h1>Hello</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About </a></li>
+                <li><a href="#">Signin</a></li>
+                <li><a href="#">Features</a></li>
+
+                <hr></hr>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header;
